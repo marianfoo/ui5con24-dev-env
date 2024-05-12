c7713718d5f364c346bb24c4ba060d3f9bc9fce4
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2024-05-12 20:47:10 +0200

    init control

---
+++ packages/control/Control.js
@@ -0,0 +1,23 @@
+sap.ui.define(["sap/ui/core/Control"], (Control) => {
+    return Control.extend("ui5con24.cc.Control", {
+        init() {
+            console.debug(`[${this.getMetadata().getName()}] > init`)
+        },
+        renderer: {
+            apiVersion: 2, // high-perf!
+            /**
+             * create the view layer
+             *
+             * @param {sap.ui.core.RenderManager} oRM Render Manager v2
+             * @param {sap.ui.core.Control} oControl this control
+             */
+            render(oRM, oControl) {
+                console.debug(`[${oControl.getMetadata().getName()}] > rendering`)
+                oRM.openStart("div", oControl)
+                oRM.openEnd()
+                // control html goes here
+                oRM.close("div")
+            }
+        }
+    })
+})
