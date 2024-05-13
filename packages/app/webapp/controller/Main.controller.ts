643f509ce3666609c98c7415c3b1fc2a5a92a1fd
„marianfoo“ <13335743+marianfoo@users.noreply.github.com>
2024-05-13 13:57:33 +0200

    app: integrate lib

--- packages/app/webapp/controller/Main.controller.ts
+++ packages/app/webapp/controller/Main.controller.ts
@@ -1,11 +1,22 @@
 import MessageBox from "sap/m/MessageBox";
 import BaseController from "./BaseController";
+import Example from "ui5con24/lib/Example";
 
 /**
  * @namespace ui5con24.app.controller
  */
 export default class Main extends BaseController {
+
+
+	public onInit(): void {
+		// const example = new Example({
+		// 	text: "Hello World!"
+		// });
+		// this.getView().addContent(example);
+	}
+
 	public sayHello(): void {
 		MessageBox.show("Hello World!");
+		
 	}
 }
